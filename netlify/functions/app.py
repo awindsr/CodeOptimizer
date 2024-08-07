83d2ebc2af3953520861c8e715a825a2c470c9a3
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-08-07 12:43:37 +0000

    fix

--- netlify/functions/app.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-
-# Set environment variable for Streamlit to find the correct port
-os.environ['PORT'] = os.getenv('PORT', '8888')
-
-# Run Streamlit app
-subprocess.run(['streamlit', 'run', 'app.py'])
